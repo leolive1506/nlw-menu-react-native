2d2c71bf5d403de25b56868ed785038abb53b443
Leonardo Lopes <leonardolivelopes2@gmail.com>
2024-02-10 17:05:01 -0300

    feat: create default layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { View, Text } from "react-native";
 
 export default function Home() {
   return (
-    <View className="bg-slate-900 flex-1">
-      <Text className="text-white">Hello react native</Text>
+    <View className="flex-1 pt-8">
+      <Text className="text-white text-2xl">Hello react native</Text>
     </View>
   )
 }
